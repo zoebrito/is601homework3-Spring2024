b3f87fe24c2ae4555c7cd00deac6c8ec3270aea1
zoebrito <zb72@njit.edu>
2024-02-14 15:22:15 -0500

    added calculations test cases

---
+++ tests/test_calculations.py
@@ -0,0 +1,22 @@
+'''
+Tests for calculations.py
+'''
+
+from calculator.operations import add, multiply, subtract, divide
+
+def test_addition():
+    '''Test that addition function works '''    
+    assert add(2,2) == 4
+
+def test_subtraction():
+    '''Test that subtraction function works '''    
+    assert subtract(2,2) == 0
+
+def test_multiplication():
+    '''Test that multiply works'''
+    assert multiply(2,2) == 4
+
+def test_division():
+    '''Test division'''
+    assert divide(2,2) == 1
+    
