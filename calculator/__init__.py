cde8c02d6f6c1180b114f7bc8c3fb8e7a816fa34
zoebrito <zb72@njit.edu>
2024-02-14 15:37:55 -0500

    added Calculator class to __init__.py

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,19 +1,25 @@
 """
-Performs addition, subtraction, multiplication, and division
+Calculator class that performs addition, subtraction, multiplication, and division
+Uses static methods meaning it doesn't require an instance to be called
 """
 
-def add(first_num, second_num):
-    """Add two numbers"""
-    return first_num + second_num
+from calculator.calculations import Calculation
+from calculator.operations import add, subtract, multiply, divide
 
-def subtract(first_num, second_num):
-    """Subtract two numbers"""
-    return first_num - second_num
-
-def multiply(first_num, second_num):
-    """Multiply two numbers"""
-    return first_num * second_num
-
-def divide(first_num, second_num):
-    """Divide two numbers"""
-    return first_num / second_num
+class Calculator:
+    @staticmethod
+    def add(first_num, second_num):
+        calculation = Calculation(first_num, second_num, add)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def subtract(first_num, second_num):
+        calculation = Calculation(first_num, second_num, subtract)  # Pass the subtract function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def multiply(first_num, second_num):
+        calculation = Calculation(first_num, second_num, multiply)  # Pass the multiply function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def divide(first_num, second_num):
+        calculation = Calculation(first_num, second_num, divide)  # Pass the divide function from calculator.operations
+        return calculation.get_result()
