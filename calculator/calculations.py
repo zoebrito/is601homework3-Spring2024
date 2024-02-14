50ff97d7a7dc26153c1df35164b571d2ce861f4b
zoebrito <zb72@njit.edu>
2024-02-14 15:13:04 -0500

    fixed pylint errors

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -1,9 +1,12 @@
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 '''
 Constructor with self parameter for calculator class
 '''
 
 class Calculation:
     def __init__(self, first_num, second_num, operation):
+        # methods automatically converted by Python into MyClass.method(myobject, arg1, arg2)
         self.first_num = first_num
         self.second_num = second_num
         self.operation = operation  # Store the operation function
@@ -11,3 +14,4 @@ class Calculation:
     def get_result(self):
         # Call the stored operation with first_num and second_num
         return self.operation(self.first_num, self.second_num)
+    
