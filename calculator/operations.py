178f8606c994c4c0fd7d4f448387d85aaa3d226c
zoebrito <zb72@njit.edu>
2024-02-14 20:54:24 -0500

    created an operations class

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,19 +1,23 @@
 '''
-Basic operations file to reference in __init__.py
+Basic operations class
 '''
+class Operations:
+    @staticmethod
+    def add(first_num, second_num):
+        """Add two numbers"""
+        return first_num + second_num
 
-def add(first_num, second_num):
-    """Add two numbers"""
-    return first_num + second_num
+    @staticmethod
+    def subtract(first_num, second_num):
+        """Subtract two numbers"""
+        return first_num - second_num
 
-def subtract(first_num, second_num):
-    """Subtract two numbers"""
-    return first_num - second_num
+    @staticmethod
+    def multiply(first_num, second_num):
+        """Multiply two numbers"""
+        return first_num * second_num
 
-def multiply(first_num, second_num):
-    """Multiply two numbers"""
-    return first_num * second_num
-
-def divide(first_num, second_num):
-    """Divide two numbers"""
-    return first_num / second_num
+    @staticmethod
+    def divide(first_num, second_num):
+        """Divide two numbers"""
+        return first_num / second_num
