38d7f10f59258ad54302ac6c381010a5724a32a9
zoebrito <zb72@njit.edu>
2024-02-14 20:57:23 -0500

    reworked

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -3,15 +3,22 @@
 '''
 Constructor with self parameter for calculator class
 '''
+from operations import Operations
 
 class Calculation:
-    def __init__(self, first_num, second_num, operation):
+    def __init__(self, first_num, second_num):
         # methods automatically converted by Python into MyClass.method(myobject, arg1, arg2)
         self.first_num = first_num
         self.second_num = second_num
-        self.operation = operation  # Store the operation function
 
-    def get_result(self):
-        # Call the stored operation with first_num and second_num
-        return self.operation(self.first_num, self.second_num)
-    
+def add(self):
+        return Operations.add(self.first_num, self.second_num)
+
+def subtract(self):
+    return Operations.subtract(self.first_num, self.second_num)
+
+def multiply(self):
+    return Operations.multiply(self.first_num, self.second_num)
+
+def divide(self):
+    return Operations.divide(self.first_num, self.second_num)
